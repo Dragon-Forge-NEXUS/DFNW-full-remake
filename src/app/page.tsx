ffacbbc931d3685773f2211a4811a90cfbbd9cbc
altie122 <77561831+altie122@users.noreply.github.com>
2024-05-06 23:33:56 +0000

    add nav buttons on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Navbar from "~/components/nav/navbar/home";
+import NavBtn from "~/components/nav/navbuttons/nexus";
 
 // const mockdata = [
 //   "hehe",
@@ -13,6 +14,15 @@ export default function HomePage() {
   return (
     <main>
       <Navbar />
+      <div className="flex md:gap-2 gap-5 flex-wrap md:container md:mx-auto p-5">
+        <NavBtn href="/" title='Code' description='DFC has coding tutorials, refrences, cheat sheets, APIs and more!'  variant={'code'}/>
+        <NavBtn href="/" title='Gaming' description='DFG contains articals on games, game reviews, gaming news, and more!'  variant={'gaming'}/>
+        {/* <NavBtn href="/" title='Media' description='DFM runs all of our media accounts.'  variant={'media'}/> */}
+        {/* <NavBtn href="/" title='News' description='DFNews has news on the tech, gaming, and programing worlds!'  variant={'news'}/> */}
+        {/* <NavBtn href="/" title='network' description='this is a network navbtn!'  variant={'network'}/> */}
+        <NavBtn href="/partners" title='Partners' description="All of DFN's partners are listed here!"  variant={'nexus'}/>
+        <NavBtn href="/blog" title='Blog' description="DFN's Blog."  variant={'nexus'}/>
+      </div>
     </main>
   );
 }
--- src/components/nav/navbuttons/nexus.tsx
+++ src/components/nav/navbuttons/nexus.tsx
@@ -1,1 +1,62 @@
-export function placeholder(){return(<></>)}
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { cva, type VariantProps } from "class-variance-authority"
+import Link from "next/link";
+import { cn } from "~/lib/utils";
+
+const buttonVariants = cva(
+  "items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-center duration-500 p-2 basis-full md:basis-1/3 lg:basis-1/4 md:flex-grow ease-in-out",
+  {
+    variants: {
+      variant: {
+        basic: 
+          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        nexus:
+          "border border-input bg-background hover:bg-dragonforge-nexus hover:text-accent-foreground",
+        code:
+          "border border-input bg-background hover:bg-dragonforge-code hover:text-accent-foreground",
+        gaming:
+          "border border-input bg-background hover:bg-dragonforge-gaming hover:text-accent-foreground",
+        media:
+          "border border-input bg-background hover:bg-dragonforge-media hover:text-accent-foreground",
+        news: 
+          "border border-input bg-background hover:bg-dragonforge-news hover:text-accent-foreground",
+        network: 
+          "border border-input bg-background hover:bg-dragonforge-network hover:text-accent-foreground",
+      },
+    },
+    defaultVariants: {
+      variant: "basic",
+    },
+  }
+)
+
+interface ButtonProps {
+  variant?: "basic" | "nexus" | "code" | "gaming" | "media" | "news" | "network" | null | undefined;
+  href: string;
+  title: string;
+  description: string;
+  isExternal?: boolean;
+};
+
+export default function NavBtn({ variant, href, isExternal, title, description }: ButtonProps) {
+  // Use a default value if variant is not provided
+  const defaultVariant = variant ?? "basic";
+
+  if (isExternal) {
+    return(
+      <Link className={cn(buttonVariants({ variant:  defaultVariant }))} href={href}>
+        <h1 className="text-xl after:content-['_↗']">{title}</h1>
+        <hr className="my-1"/>
+        <p>{description}</p>
+      </Link>
+    );
+  } else {
+    return(
+      <Link className={cn(buttonVariants({ variant:  defaultVariant }))} href={href}>
+        <h1 className="text-xl">{title}</h1>
+        <hr className="my-1"/>
+        <p>{description}</p>
+      </Link>
+    );
+  }
+}
