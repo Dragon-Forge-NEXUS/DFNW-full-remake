3e26171dee2ba52c4bde50ae3f625e49dbaabcb9
altie122 <77561831+altie122@users.noreply.github.com>
2024-05-07 01:08:00 +0000

    fix partners pages text positioning

--- src/app/partners/fyts/page.tsx
+++ src/app/partners/fyts/page.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <main>
       <Navbar />
-      <div className="flex md:gap-2 gap-5 flex-wrap md:container md:mx-auto p-5">
+      <div className="md:container md:mx-auto p-5">
         <h1 className="text-2xl">🚧 Pardon Our dust 🚧</h1>
         <p>We are working on getting this page build.</p>
       </div>
--- src/app/partners/page.tsx
+++ src/app/partners/page.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <main>
       <Navbar />
-      <div className="flex md:gap-2 gap-5 flex-wrap md:container md:mx-auto p-5">
+      <div className="md:container md:mx-auto p-5">
         <h1 className="text-2xl">DragonForgeNEXUS Partnerships</h1>
         <p>DragonForgeNEXUS partnerships are FYTS members and sponserships.<br />As of now we do not have any sponsors, thus the page is not available.</p>
       </div>
